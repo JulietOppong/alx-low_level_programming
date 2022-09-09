f17558e3d584b6f886267adc3ac0ccd2ec3fb6c8
Oppong Juiet Oppong <akuyhanana5@gmail.com>
2022-09-08 18:57:19 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,8 @@
 
 /**
  *
+ * main - print the string in the put function
+ *
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
