aad526f3309e0101a19bf2c8c1b4be7a7f34c59a
Oppong Juiet Oppong <akuyhanana5@gmail.com>
2022-09-13 19:18:03 -0700

    Task 11

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer - function that prints every minute of the day 
+ * jack_bauer - function that prints every minute of the day
  * n from 00:00 to 23, min loop counts min, while hour loop counts hours
  * and resets mins
  * Return: 0
